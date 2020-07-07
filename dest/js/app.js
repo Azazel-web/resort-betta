0b342850256424963386556103a31d3b87efa836
Danil <alkolpashikov@bk.ru>
2020-07-07 22:06:39 +0300

    5

--- dest/js/app.js
+++
@@ -1,8 +0,0 @@
-let accardionTitle = document.querySelectorAll('.accardion__title')
-
-accardionTitle.forEach(elem => elem.addEventListener('click', () => {
-   if(elem.nextElementSibling.style.display === 'none'){
-      elem.nextElementSibling.style.display = 'block'
-   } else
-   elem.nextElementSibling.style.display = 'none'
-}))
